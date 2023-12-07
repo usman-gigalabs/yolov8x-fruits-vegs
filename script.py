6b88a585e8e5b9af2c4422f2d7285d993167fc64
Usman <usman.faisal@gigalabs.co>
2023-12-07 16:39:33 +0500

    update model

--- script.py
+++ script.py
@@ -3,5 +3,5 @@ checks()
 
 hub.login('ab084fa7d461a2674af6880e5199604533945d2634')
 
-model = YOLO('https://hub.ultralytics.com/models/tD9zO1RvV0j1xnU2bev5')
+model = YOLO('https://hub.ultralytics.com/models/T4sZgzaD4gSkmZbYcxZa')
 results = model.train()
